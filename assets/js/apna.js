5ecc2a4883690b0b9e6b7c3fe698c71ec8a269aa
Mohib-Digital <mohibullahafridi2020@gmail.com>
2024-06-01 14:41:20 -0700

    add codes and files

--- assets/js/apna.js
+++ assets/js/apna.js
@@ -1,10 +1,51 @@
+// Chapter.No: 09 (prompts)
+// prompts are used to take user inputs
+// prompts always returns string values
 
-var cardMoney = +prompt("Enter your card")
+var age = prompt("Enter you age!")
+alert("You are " + age + " years old.")
+// =====================================================================================================
 
-if (cardMoney >= 50){
-alert("You can go now!")
-}
+// var userName = prompt("Enter your name")
+// alert("Hello! " + userName)
+// ====================================================================================================
 
-if (cardMoney < 50){
-    alert("Sorry! You have to recharge")
-}
+// var firstName = prompt("Please enter you first name:")
+// var lastName = prompt("Please enter your last name:")
+// var fullName = firstName + " " + lastName;
+// alert("Hello! " + fullName);
+// =====================================================================================================
+
+// var pencils = +prompt("Price for Pencils:");
+// var notebooks = +prompt("Price for NoteBooks");
+// var totalBill = pencils + notebooks;
+// alert("Your total bill is: " + totalBill)
+// ===================================================================================================== 
+
+// BRT Card Program!
+// var cardMoney = +prompt("Enter your card");
+
+// if (cardMoney >= 50){
+// alert("You can go now!")
+// }
+
+// if (cardMoney < 50){
+//     alert("Sorry! You have to recharge!")
+// }
+
+// else {
+//     alert("Sorry! You have to recharge!")
+// }
+// =====================================================================================================
+
+// var balls = prompt("How many balls?");
+// var extraBalls = 5;
+// var toMuchBalls = balls + extraBalls;
+// alert(toMuchBalls);
+
+// var balls = +prompt("How many balls?")
+// var extraBalls = 5;
+// var toMuchBalls = balls + extraBalls;
+// alert(toMuchBalls);
+// =====================================================================================================
+
