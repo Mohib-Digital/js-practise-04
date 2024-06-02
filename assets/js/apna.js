13a3cd1b2227d299352ffc766f67651723f96e5b
Mohib-Digital <mohibullahafridi2020@gmail.com>
2024-06-02 00:05:40 -0700

    add code and files

--- assets/js/apna.js
+++ assets/js/apna.js
@@ -2,8 +2,8 @@
 // prompts are used to take user inputs
 // prompts always returns string values
 
-var age = prompt("Enter you age!")
-alert("You are " + age + " years old.")
+// var age = prompt("Enter you age!")
+// alert("You are " + age + " years old.")
 // =====================================================================================================
 
 // var userName = prompt("Enter your name")
@@ -48,4 +48,146 @@ alert("You are " + age + " years old.")
 // var toMuchBalls = balls + extraBalls;
 // alert(toMuchBalls);
 // =====================================================================================================
+
+// Chapter.No: 10    (if statement)
+
+// var badalHai = true;
+
+// if (badalHai){
+//     alert("Chatrai zan sara rawala")
+// }
+// else {
+//     alert("Bsss pregda wa yara")
+// }
+// ======================================================================================================
+
+// driving license programe
+// var userAge = +prompt("Enter your age please!");
+// if (userAge === 18){
+//     alert("You are eligible to get your driving license")
+// }
+
+// else if (userAge > 18){
+//     alert("You are aligible to get your driving license")
+// }
+// else {
+//     alert("You are not aligible to get your driving license")
+// }
+// =======================================================================================================
+
+// uni town sy saddar jani ka kraya dene hai Rs.40
+// var gadiKakarya = +prompt("Karaya dedo bhai!")
+
+// if (gadiKakarya === 40){
+//     alert("Buhat shukriya bhai karaya dene ka")
+// }
+// if (gadiKakarya > 40){
+//     alert("Bhai zeyada kharcha mat krO serf 40 rupees dedo 🤞")
+// }
+// if (gadiKakarya !== 40){
+//     alert("Bhai naraz mat hu liken Gadi petrol pe chalthe hai pani pe nahi 😁")
+// }
+
+
+// a porgram weather a number is positive, zero or negative
+var userName = prompt("Enter your name please!")
+var userNumber = +prompt("Enter any number please")
+
+if (userNumber === 0){
+    alert(userName + " Ap ni 0 enter kia")   
+}
+if (userNumber > 0){
+    alert(userName + " Ap ni positive number enter kia")
+}
+if (userNumber < 0){
+    alert(userName + " Ap ne negative number enter kia")
+}
+// =================================================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
